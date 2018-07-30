4a3b19ae4adad1bf69c93a5cf7f5712aecd4c7e8
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-07-30 14:30:42 +0200

    Initial commit- Iteration 1. Create object rover

---
+++ javascripts/app.js
@@ -0,0 +1,18 @@
+// Rover Object Goes Here
+// ======================
+rover {
+  direction: N
+}
+
+// ======================
+function turnLeft(rover){
+  console.log("turnLeft was called!");
+}
+
+function turnRight(rover){
+  console.log("turnRight was called!");
+}
+
+function moveForward(rover){
+  console.log("moveForward was called")
+}
