04315d1a6c094d09b904be0acb1da8eaf4cf9082
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-07-31 13:23:45 +0200

    Iteration 2 - Correct several misspellings - Implement turnRight and turnLeft functions

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,16 +1,54 @@
 // Rover Object Goes Here
 // ======================
-rover {
-  direction: N
+var rover = {
+  direction: "N"
 }
 
 // ======================
+
 function turnLeft(rover){
   console.log("turnLeft was called!");
+  switch (rover.direction) {
+    case "N":
+      rover.direction = "W";
+      break;
+
+    case "E":
+      rover.direction = "N";
+      break;
+
+    case "S":
+      rover.direction = "E";
+      break;
+    
+    case "W":
+      rover.direction = "S";
+      break;
+  }
+  console.log("Current position: " + rover.direction)
 }
+
 
 function turnRight(rover){
   console.log("turnRight was called!");
+  switch (rover.direction) {
+    case "N":
+      rover.direction = "E";
+      break;
+
+    case "E":
+      rover.direction = "S";
+      break;
+
+    case "S":
+      rover.direction = "W";
+      break;
+    
+    case "W":
+      rover.direction = "N";
+      break;
+  }
+  console.log("Current position: " + rover.direction)
 }
 
 function moveForward(rover){
