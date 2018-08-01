73939e91e87ce37dfcd2e3fdeabf31c4485654a1
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-08-01 12:36:45 +0200

    Bonus Iteration - Rover can not move beyond map limits (x10, y10). Implement moveBackwards function

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,7 +3,8 @@
 var rover = {
   direction: "N",
   x: 0,
-  y: 0
+  y: 0,
+  travelLog: []
 }
 
 // ======================
@@ -56,18 +57,83 @@ function turnRight(rover){
 function moveForward(rover){
   console.log("moveForward was called");
   switch (rover.direction) {
+    
     case "N":
+      if (rover.y === 0) {
+      console.error("Can not move to negative coordinates");
+      break;
+      } else {
       rover.y -= 1;
       break;
+      }
+
     case "E":
+      if (rover.x === 10) {
+      console.error("Can not move beyond the limit of the map (x:10, y:10)");
+      break;
+      } else {
       rover.x += 1;
       break;
+      }
+
     case "S":
+      if (rover.y === 10) {
+      console.error("Can not move beyond the limit of the map (x:10, y:10)");
+      break;
+      }
       rover.y += 1;
       break;
+
     case "W":
+      if (rover.x === 0) {
+        console.error("Can not move to negative coordinates");
+        break;
+      } else {
       rover.x -= 1;
       break;
+      }
+  }
+  console.log("Current position: X=" + rover.x + " Y=" + rover.y);
+}
+
+function moveBackwards(rover) {
+  console.log("moveBackwards was called");
+  switch (rover.direction) {
+    
+    case "N":
+      if (rover.y === 10) {
+      console.error("Can not move beyond the limit of the map (x:10, y:10)");
+      break;
+      } else {
+      rover.y += 1;
+      break;
+      }
+
+    case "E":
+      if (rover.x === 0) {
+      console.error("Can not move to negative coordinates");
+      break;
+      } else {
+      rover.x -= 1;
+      break;
+      }
+
+    case "S":
+      if (rover.y === 0) {
+      console.error("Can not move to negative coordinates");
+      break;
+      }
+      rover.y -= 1;
+      break;
+
+    case "W":
+      if (rover.x === 10) {
+        console.error("Can not move to negative coordinates");
+        break;
+      } else {
+      rover.x += 1;
+      break;
+      }
   }
   console.log("Current position: X=" + rover.x + " Y=" + rover.y);
 }
@@ -84,7 +150,16 @@ function movementSequence(sequence) {
       case "l":
         turnLeft(rover);
         break;
+      case "b":
+        moveBackwards(rover);
+        break;
     }
-  } 
-return "ready to go again"  
+  }   
+  rover.travelLog.push([rover.x, rover.y]);
+  //Travel Log
+  console.log("Travel Log:");
+  for (let elm of rover.travelLog) {
+    console.log("[" + elm + "] ");
+  }
+  return "ready to go again"
 }
